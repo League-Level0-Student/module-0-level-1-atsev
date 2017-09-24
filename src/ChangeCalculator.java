648297e2c0f390b800c33f381cc0a974f35d3b02
atsev <league@WTS21.local>
2017-09-24 16:01:24 -0700

    ChangeCalculator

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -17,15 +17,19 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// 1. Ask the user how many nickels they have
-
+String a = JOptionPane.showInputDialog("How many nickels do you have?");
+int b = Integer.parseInt(a);
 		// 2. Convert their answer to an int using Integer.parseInt()
-
 		// 3. Ask the user how many dimes they have, and convert their answer
-
+String c = JOptionPane.showInputDialog("How many dimes do you have?");
+int d = Integer.parseInt(c);
 		// 4. Ask the user how many quarters they have, and convert their answer
 
+String e = JOptionPane.showInputDialog("How many quarters do you have?");
+int f = Integer.parseInt(e);
 		// 5. Calculate how much money the user has and save it in a double variable 
-
+int q = (b*5) + (d*10) + (f*25);
+JOptionPane.showMessageDialog(null, q + " cents");
 		// 6. Tell the user how much money they have
 
 	}
